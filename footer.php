73c9685560ba04a22c526d9e85702b5a3932e170
janice92 <janice.mette@gmail.com>
2016-09-21 18:54:16 +0200

    footer

---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+<meta charset="utf-8">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+<script rel="stylesheet" type="text/javascript" src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+<script rel="stylesheet" type="text/javascript" src="bootstrap-3.3.7-dist/js/npm.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<head>
+	<title>Classement des 10 meilleurs anime</title>
+</head>
+<footer>
+<span class="copyright">Top Anime &copy; 2016</span>
+	
+</footer>
