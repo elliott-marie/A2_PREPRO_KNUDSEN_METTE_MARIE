c58dc0a4e4f4626dfa87f4afe6dd4e68990047d3
janice92 <janice.mette@gmail.com>
2016-09-22 23:04:31 +0200

    new

--- footer.php
+++ footer.php
@@ -7,11 +7,23 @@
 <script rel="stylesheet" type="text/javascript" src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
 <script rel="stylesheet" type="text/javascript" src="bootstrap-3.3.7-dist/js/npm.js"></script>
 <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<link type="text/css" rel="stylesheet" href="path-to-fontawesome/font-awesome.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <head>
 	<title>Classement des 10 meilleurs anime</title>
 </head>
 <footer>
 <span class="copyright">Top Anime &copy; 2016</span>
-	
+	<div class="socialbtns" style="    margin-left: 44%;
+    margin-top: -18px;">
+    <ul>
+        <li><a href="#" class="fa fa-lg fa-facebook"></a></li>
+        <li><a href="#" class="fa fa-lg fa-twitter"></a></li>
+        <li><a href="#" class="fa fa-lg fa-google-plus"></a></li>
+        <li><a href="#" class="fa fa-lg fa-instagram"></a></li>
+   
+        <li><a href="#" class="fa fa-lg fa-youtube"></a></li>
+
+    </ul>
+</div>
 </footer>
---
+++ form.php
@@ -0,0 +1,48 @@
+<?php
+
+?>
+<meta charset="utf-8">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+<script rel="stylesheet" type="text/javascript" src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+<script rel="stylesheet" type="text/javascript" src="bootstrap-3.3.7-dist/js/npm.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<link type="text/css" rel="stylesheet" href="path-to-fontawesome/font-awesome.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<head>
+	<title>Classement des 10 meilleurs anime</title>
+</head>
+
+<body>
+	
+		  <section id="contact-form">
+		  	<div class="carre">
+  <h2>Contact</h2>
+  <form id="contact" name="contact" accept-charset="utf-8">
+  	<form method="post" action="traitement.php">
+   <p>
+       <label for="pays">Dans quel pays habitez-vous ?</label><br />
+       <select name="pays" id="pays">
+           <option value="france">France</option>
+           <option value="espagne">Espagne</option>
+           <option value="italie">Italie</option>
+           <option value="royaume-uni">Royaume-Uni</option>
+           <option value="canada">Canada</option>
+           <option value="etats-unis">États-Unis</option>
+           <option value="chine">Chine</option>
+           <option value="japon">Japon</option>
+       </select>
+   </p>
+</form>
+    <label><span>Name</span><input name="name" type="text" placeholder="Name"/></label>
+    <label><span>Email</span><input name="email" type="email" placeholder="Email"/></label>
+    <label><span>Message</span><textarea name="message" placeholder="Message"></textarea></label>
+    <input name="submit" type="submit" value="Send"/>
+  </form>
+  
+  </div>
+</section>
+  
+	
+</body>
+<?php include('footer.php'); ?>
